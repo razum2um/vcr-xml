90496e59fb135abcef5f568eab2326ccb6943168
Vlad Bokov <razum2um@mail.ru>
2013-07-26 11:30:07 +0700

    another fix

--- lib/vcr/cassette/serializers/xsyck.rb
+++ lib/vcr/cassette/serializers/xsyck.rb
@@ -3,28 +3,15 @@ module VCR
     class Serializers
       module Xsyck
         extend EncodingErrorHandling
+        extend Syck
         extend self
-
-        ENCODING_ERRORS = [ArgumentError]
-
-        def file_extension
-          "yml"
-        end
-
-        def deserialize(string)
-          handle_encoding_errors do
-            ::Syck.load(string)
-          end
-        end
 
         def serialize(hash)
           hash['http_interactions'].each do |i|
             pretty_xml i['request']['body']['string'] if /<\?xml/.match i['request']['body']['string']
             pretty_xml i['response']['body']['string'] if /<\?xml/.match i['response']['body']['string']
           end
-          handle_encoding_errors do
-            ::Syck.dump(hash)
-          end
+          super(hash)
         end
 
         private
